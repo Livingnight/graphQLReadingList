0d9135eb9581719426ae452fe2b9cf82900cb629
unknown <jjernigan1065@gmail.com>
2018-07-27 18:57:08 -0400

    basic graphql server setup, beginning work on front end with react

--- server/server.js
+++ server/server.js
@@ -2,17 +2,20 @@ const express = require('express');
 const grapqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const password = process.env.DBPASSWORD;
+const user = process.env.DBUSER;
 app.use('/graphql', grapqlHTTP({
     schema,
     graphiql: true
 }));
 
-mongoose.connect('mongodb://livingnight:password41@ds259620.mlab.com:59620/gql-livingnight');
+mongoose.connect(`mongodb://${user}:${password}@ds259620.mlab.com:59620/gql-livingnight`);
 mongoose.connection.once('open', () => {
     console.log('connected to database');
 })
