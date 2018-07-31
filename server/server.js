976a8c9777657ced82f1d6b0afac4296208fdfd2
unknown <jjernigan1065@gmail.com>
2018-07-31 17:50:37 -0400

    added scripts and and static paths for heroku deployment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const grapqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const path = require('path');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -10,13 +11,25 @@ const app = express();
 //allow cross origin requests
 app.use(cors());
 
+
+if(process.env.NODE_ENV) {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname +'/client/build/index.html'));
+    });
+}
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname +'/client/build/index.html'));
+});
+
 const PORT = process.env.PORT || 3001;
 
 app.use('/graphql', grapqlHTTP({
     schema,
     graphiql: true
 }));
-mongoose.connect(process.env.DB_URL);
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true});
 mongoose.connection.once('open', () => {
     console.log('connected to database');
 })
