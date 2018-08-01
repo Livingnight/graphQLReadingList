f63b2942eb440172140f93017949ba722da3fa9d
unknown <jjernigan1065@gmail.com>
2018-07-31 20:43:58 -0400

    building for production

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 
 console.log(__dirname, 'client/build');
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
